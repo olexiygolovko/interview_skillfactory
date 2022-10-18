a7722d5be4138b353e4eb8534daf40083f0f0e71
olexiygolovko <olexiygolvoko92@mai.ru>
2022-10-18 20:56:52 +0300

    finished task

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Программа подсчета слов в файле
+import os
+import sys
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(
+f'Вам доступны следующие команды:\n',
+f'1) load  — загрузить слова из файла filename.txt;\n',
+f'2) wordcount проверочное слово — отобразить число раз, которое программа встретила слово\n'
+f'3) clear-memory — очистить все данные о прочитанных словах из памяти.\n'
+)
+command = input("Введите комманду:")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def get_words(filename):
+    with open(filename, encoding="utf8") as file:
+        text = file.read()
+    text = text.replace("\n", " ")
+    text = text.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
+    text = text.lower()
+    words = text.split()
+    words.sort()
+    return words
+
+
+def get_words_dict(words):
+    words_dict = dict()
+
+    for word in words:
+        if word in words_dict:
+            words_dict[word] = words_dict[word] + 1
+        else:
+            words_dict[word] = 1
+    return words_dict
+
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+def main():
+    if command == 'load':
+        filename = input("Введите путь к файлу: ")
+        if not os.path.exists(filename):
+            print("Указанный файл не существует")
+        else:
+            words = get_words(filename)
+            words_dict = get_words_dict(words)
+            print(f"Кол-во слов: {len(words)}")
+            print(f"Кол-во уникальных слов: {len(words_dict)}")
+            print("Все использованные слова:")
+            for word in words_dict:
+                print(word.ljust(20), words_dict[word])
+    if command == 'clear-memory':
+            restart_program()
+
+
+if __name__ == "__main__":
+    main()
